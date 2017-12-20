22e3b7f501afef44b71e42160c9f3111c16ce12a
jdvera <jdvera2015@gmail.com>
2017-12-20 10:02:01 -0600

    initial docs

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var methodOverride = require("methos-override");
+var bodyParser = require("body-parser");
+
+var app = express();
+var port = 3007;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "wishes_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
